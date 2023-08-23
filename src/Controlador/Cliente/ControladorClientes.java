d521a180030db15aa7e85845001ccbd5775dbfd2
Thailyn Parrales <shailyn04parrales@gmail.com>
2023-08-23 11:48:46 -0600

    Clase controlador cliente
    Se modifico

--- src/Controlador/Cliente/ControladorClientes.java
+++ src/Controlador/Cliente/ControladorClientes.java
@@ -9,5 +9,38 @@ package Controlador.Cliente;
  * @author Student
  */
 public class ControladorClientes {
+    private int edad;
+    private String Discapacidad;
+    private String op;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDiscapacidad() {
+        return Discapacidad;
+    }
+
+    public void setDiscapacidad(String Discapacidad) {
+        this.Discapacidad = Discapacidad;
+    }
+
+    public String getOp() {
+        return op;
+    }
+
+    public void setOp(String op) {
+        this.op = op;
+    }
+
+    public ControladorClientes(int edad, String Discapacidad, String op) {
+        this.edad = edad;
+        this.Discapacidad = Discapacidad;
+        this.op = op;
+    }
     
 }
